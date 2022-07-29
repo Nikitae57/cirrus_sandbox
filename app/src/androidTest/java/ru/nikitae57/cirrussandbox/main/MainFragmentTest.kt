5c7b9f2f00ab220e2ab2e5095d1c540723df14ee
Nikita Evdokimov <nikita-e57@yandex.ru>
2022-07-29 18:03:09 +0300

    Trying pulling screenshots

--- app/src/androidTest/java/ru/nikitae57/cirrussandbox/main/MainFragmentTest.kt
+++ app/src/androidTest/java/ru/nikitae57/cirrussandbox/main/MainFragmentTest.kt
@@ -52,7 +52,7 @@ class MainFragmentTest : DocLocScreenshotTestCase(
 
         captureScreenshot("initial_state")
         MainScreen {
-            button { isDisplayed() }
+            button { isDisplayed() } //
             label { isNotDisplayed() }
         }
     }
